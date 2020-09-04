5f0fc6759b5264fcb8d886363fc19727f4804092
zhangzhihai <13504984520@139.com>
2020-09-04 11:57:57 +0800

    splash

--- app/src/main/java/com/usher/demo/MainActivity.kt
+++ app/src/main/java/com/usher/demo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.usher.demo
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import com.google.gson.Gson
+import com.usher.demo.api.ApiFactory
+import com.usher.demo.base.BaseActivity
+import com.usher.demo.util.LogUtil
+import com.usher.demo.util.RxUtil
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    override fun initView() {
+        intent.getStringExtra(Constants.TAG_URL)?.let {
+            startActivity(Intent(this, CommonWebActivity::class.java).apply {
+                putExtra(Constants.TAG_URL, it)
+            })
+        }
+
+        ApiFactory.instance.getDetail()
+                .to(RxUtil.autoDispose(this))
+                .subscribe { result ->
+                    result.data?.run {
+                        LogUtil.log("data: ${Gson().toJson(this)}")
+                    }
+                }
+
+//        showLoadingDialog()
     }
 }
--- app/src/main/java/com/usher/demo/SplashActivity.kt
+++ app/src/main/java/com/usher/demo/SplashActivity.kt
@@ -1,6 +1,51 @@
 package com.usher.demo
 
+import android.content.Intent
+import android.view.View
+import com.jakewharton.rxbinding4.view.clicks
 import com.usher.demo.base.BaseActivity
+import com.usher.demo.util.PermissionUtil
+import com.usher.demo.util.RxUtil
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.concurrent.TimeUnit
 
 class SplashActivity : BaseActivity(R.layout.activity_splash) {
+    companion object {
+        private const val COUNTDOWN_SECONDS = 3L
+        private const val URL = "https://www.7fresh.com/"
+    }
+
+    private var mSplashUrl: String? = null
+
+    override fun initView() {
+        val countdowns = Observable.interval(0, 1000, TimeUnit.MILLISECONDS)
+                .compose(RxUtil.getSchedulerComposer())
+                .take(COUNTDOWN_SECONDS + 1)
+                .map { COUNTDOWN_SECONDS - it }
+                .doOnNext { skip_textview.text = getString(R.string.splash_skip, it) }
+
+        val adClicks = ad_imageview.clicks().take(1).doOnNext { mSplashUrl = URL }
+        val skipClicks = skip_textview.clicks().take(1)
+
+        PermissionUtil.requestPermissions(this, Constants.PERMISSION_INIT_APP)
+                .take(1)
+                .switchMap {
+                    ad_imageview.visibility = View.VISIBLE
+                    skip_textview.visibility = View.VISIBLE
+                    countdowns
+                }
+                .takeUntil { it <= 0 }
+                .takeUntil(adClicks)
+                .takeUntil(skipClicks)
+                .doOnComplete {
+                    startActivity(Intent(this, MainActivity::class.java).apply {
+                        putExtra(Constants.TAG_URL, mSplashUrl)
+                    })
+
+                    finish()
+                }
+                .to(RxUtil.autoDispose(this))
+                .subscribe { }
+    }
 }
