46282a1667cdab6cc747e20acb040d41a709f6aa
zhangzhihai <13504984520@139.com>
2020-09-04 12:11:44 +0800

    main (exit)

--- app/src/main/java/com/usher/demo/MainActivity.kt
+++ app/src/main/java/com/usher/demo/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.usher.demo
 
 import android.content.Intent
+import android.view.KeyEvent
+import androidx.fragment.app.Fragment
 import com.google.gson.Gson
 import com.usher.demo.api.ApiFactory
 import com.usher.demo.base.BaseActivity
 import com.usher.demo.util.LogUtil
 import com.usher.demo.util.RxUtil
+import io.reactivex.rxjava3.subjects.PublishSubject
 
-class MainActivity : BaseActivity(R.layout.activity_main) {
+class MainActivity : BaseActivity(R.layout.activity_main, Theme.LIGHT_AUTO) {
+    companion object {
+        private const val EXIT_DURATION = 1000
+    }
 
-    override fun initView() {
+    private val mFragmentMap = hashMapOf<String, Fragment>()
+    private val mBackKey = PublishSubject.create<Unit>()
+
+    override fun initData() {
         intent.getStringExtra(Constants.TAG_URL)?.let {
             startActivity(Intent(this, CommonWebActivity::class.java).apply {
                 putExtra(Constants.TAG_URL, it)
             })
         }
+    }
+
+    override fun initView() {
+
+        val intervals = mBackKey.timeInterval().map { it.time() }
+        val exits = intervals.skip(1).filter { it < EXIT_DURATION }
+
+        exits.to(RxUtil.autoDispose(this)).subscribe { finish() }
+
+        intervals.takeUntil(exits)
+                .to(RxUtil.autoDispose(this))
+                .subscribe { showToast("再次点击退出程序") }
+
 
         ApiFactory.instance.getDetail()
                 .to(RxUtil.autoDispose(this))
@@ -23,7 +45,14 @@ class MainActivity : BaseActivity(R.layout.activity_main) {
                         LogUtil.log("data: ${Gson().toJson(this)}")
                     }
                 }
+    }
 
-//        showLoadingDialog()
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            mBackKey.onNext(Unit)
+            return false
+        }
+
+        return super.onKeyDown(keyCode, event)
     }
 }
