508f1620fd2b14d70079d347d9864fc3ef4c2994
zhangzhihai <13504984520@139.com>
2020-09-05 15:27:51 +0800

    main web

--- app/src/main/java/com/usher/demo/main/fragment/MainProductFragment.kt
+++ app/src/main/java/com/usher/demo/main/fragment/MainProductFragment.kt
@@ -1,5 +1,8 @@
 package com.usher.demo.main.fragment
 
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import android.webkit.WebView
 import com.google.gson.Gson
 import com.usher.demo.R
 import com.usher.demo.api.ApiFactory
@@ -7,6 +10,7 @@ import com.usher.demo.base.BaseNavigationFragment
 import com.usher.demo.util.LogUtil
 import com.usher.demo.util.RxUtil
 import kotlinx.android.synthetic.main.fragment_main_product.*
+import kotlinx.android.synthetic.main.main_delivery_layout.*
 import kotlinx.android.synthetic.main.main_description_layout.*
 
 class MainProductFragment : BaseNavigationFragment(R.layout.fragment_main_product) {
@@ -14,8 +18,8 @@ class MainProductFragment : BaseNavigationFragment(R.layout.fragment_main_produc
         fun newInstance() = MainProductFragment()
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun init() {
-
         ApiFactory.instance.getDetail()
                 .to(RxUtil.autoDispose(this))
                 .subscribe { result ->
@@ -27,6 +31,21 @@ class MainProductFragment : BaseNavigationFragment(R.layout.fragment_main_produc
                         description_textview.text = adText
                         price_textview.text = getString(R.string.main_price, jdPrice)
                         unit_textview.text = buyUnit
+
+                        full_address_textview.text = getString(R.string.main_full_address, jdShipment.address)
+                        address_textview.text = jdShipment.address
+                        date_textview.text = jdShipment.deliveryDateAndTime
+
+                        val webView = WebView(requireContext()).apply {
+                            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                            settings.run {
+                                javaScriptEnabled = true
+                                domStorageEnabled = true
+                            }
+
+                            loadUrl(detailUrl)
+                        }
+                        content_layout.addView(webView)
                     }
                 }
     }
