b97a247d96a999ad4b68bf88c2acbf5f4f56f32a
zhangzhihai <13504984520@139.com>
2020-09-05 12:17:48 +0800

    main banner

--- app/src/main/java/com/usher/demo/main/widget/BannerView.kt
+++ app/src/main/java/com/usher/demo/main/widget/BannerView.kt
@@ -3,14 +3,18 @@ package com.usher.demo.main.widget
 import android.content.Context
 import android.os.Bundle
 import android.util.AttributeSet
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.LifecycleOwner
+import com.jakewharton.rxbinding4.viewpager.pageSelections
 import com.squareup.picasso.Picasso
 import com.usher.demo.Constants
 import com.usher.demo.R
 import com.usher.demo.base.BasePagerFragment
+import com.usher.demo.util.RxUtil
 import com.usher.demo.widget.SquareLayout
 import com.usher.demo.widget.pager.LoopViewPager
 import kotlinx.android.synthetic.main.fragment_main_banner.*
@@ -24,12 +28,22 @@ class BannerView @JvmOverloads constructor(context: Context, attrs: AttributeSet
             field = value
             mAdapter.data = value
             mAdapter.notifyDataSetChanged()
+            banner_viewpager.currentItem = 0
+
+            visibility = if (value.isNotEmpty()) VISIBLE else GONE
         }
 
     init {
         inflate(context, R.layout.main_banner_layout, this)
+        visibility = View.INVISIBLE
 
         banner_viewpager.adapter = mAdapter
+        banner_viewpager.pageSelections()
+                .filter { data.isNotEmpty() }
+                .to(RxUtil.autoDispose(context as LifecycleOwner))
+                .subscribe { position ->
+                    indicator_textview.text = context.getString(R.string.banner_indicator, position + 1, data.size)
+                }
     }
 
     private class BannerFragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
