9f0b4c384514321d31b0450cc67585c1e20be2e9
zhangzhihai <13504984520@139.com>
2020-09-08 12:14:53 +0800

    order address

--- app/src/main/java/com/usher/demo/order/fragment/OrderDeliveryFragment.kt
+++ app/src/main/java/com/usher/demo/order/fragment/OrderDeliveryFragment.kt
@@ -1,7 +1,10 @@
 package com.usher.demo.order.fragment
 
+import com.jakewharton.rxbinding4.view.clicks
 import com.usher.demo.R
 import com.usher.demo.base.BaseNavigationFragment
+import com.usher.demo.util.RxUtil
+import kotlinx.android.synthetic.main.fragment_order_delivery.*
 
 class OrderDeliveryFragment : BaseNavigationFragment(R.layout.fragment_order_delivery) {
     companion object {
@@ -9,5 +12,9 @@ class OrderDeliveryFragment : BaseNavigationFragment(R.layout.fragment_order_del
     }
 
     override fun init() {
+        address_layout.clicks()
+            .compose(RxUtil.singleClick())
+            .to(RxUtil.autoDispose(this))
+            .subscribe { showToast("更换地址") }
     }
 }
